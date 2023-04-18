e95cb1be4e2df70d704f4a703dfa3ec0e7871d66
Sandev <sandevdewthilina2000@gmail.com>
2023-04-18 20:09:07 +0530

    external api update

--- HRMS_WEB/ApiControllers/ExternalApiController.cs
+++ HRMS_WEB/ApiControllers/ExternalApiController.cs
@@ -15,6 +15,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Net.Http.Headers;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace HRMS_WEB.ApiControllers
 {
@@ -30,7 +31,7 @@ namespace HRMS_WEB.ApiControllers
         private readonly ITableExtractor _tableExtractor;
 
         public ExternalApiController(
-            HRMSDbContext db, 
+            HRMSDbContext db,
             IStorageRepository storageRepository,
             IScraper scraper,
             ITableExtractor tableExtractor)
@@ -72,11 +73,7 @@ namespace HRMS_WEB.ApiControllers
                     uploadDataList.Add(await ProcessUploadForIdSync(upload.ID, SupplierId));
                 }
 
-                return Json(new
-                {
-                    success = true,
-                    data = uploadDataList
-                });
+                return Json(FormatResults(uploadDataList));
             }
             catch (Exception e)
             {
@@ -87,24 +84,115 @@ namespace HRMS_WEB.ApiControllers
                 });
             }
         }
-        
+
+        private object FormatResults(List<UploadData> uploadDataList)
+        {
+            var result = uploadDataList.FirstOrDefault();
+            var tableComponents = _db.TableComponent.Where(t => t.TemplateID == result.DetectedTemplateID).OrderBy(t => t.ID).ToList();
+            var map = new Dictionary<string, object>();
+            if (result != null)
+            {
+                // Convert the JSON string to a Dictionary<string, JToken>
+                var fieldJsonArray = JArray.Parse(result.FieldJson);
+
+                foreach (JObject jsonObject in fieldJsonArray.Children<JObject>())
+                {
+                    map.Add(jsonObject.GetValue("Key")!.ToString(), jsonObject.GetValue("Value")!.ToString());
+                }
+
+                var tableJsonArray = JArray.Parse(result.TableJson);
+
+                try
+                {
+                    // Get the dimensions of the 3D array
+                    int dim1 = tableJsonArray.Count;
+
+                    List<List<List<string>>> list3d = new List<List<List<string>>>();
+
+                    for (int i = 0; i < dim1; i++)
+                    {
+                        int dim2 = ((JArray) tableJsonArray[i]).Count;
+                        List<List<string>> tableList = new List<List<string>>();
+                        for (int j = 0; j < dim2; j++)
+                        {
+                            int dim3 = ((JArray) tableJsonArray[i][j]).Count;
+                            List<string> rowList = new List<string>();
+                            for (int k = 0; k < dim3; k++)
+                            {
+                                rowList.Add((string) tableJsonArray[i][j][k]);
+                            }
+
+                            tableList.Add(rowList);
+                        }
+
+                        list3d.Add(tableList);
+                    }
+
+                    int tableIndex = 0;
+                    foreach (List<List<string>> table in list3d)
+                    {
+                        List<Dictionary<string, string>> tableRows = new List<Dictionary<string, string>>();
+
+                        bool hasPredefinedHeadings = !string.IsNullOrEmpty(tableComponents.ElementAt(tableIndex).Headings);
+                        
+                        var tableHeadings = table[0];
+                        var tableContent = table.GetRange(1, table.Count - 1);
+                        
+                        if (hasPredefinedHeadings)
+                        {
+                            tableHeadings = tableComponents.ElementAt(tableIndex).Headings.Split("|").ToList();
+                            tableContent = table;
+                        }
+                        
+                        
+                        for (int i = 0; i < tableContent.Count; i++)
+                        {
+                            Dictionary<string, string> kvp = new Dictionary<string, string>();
+                            for (int j = 0; j < tableContent[i].Count; j++)
+                            {
+                                var key = tableHeadings[j];
+                                if (string.IsNullOrEmpty(key) || string.IsNullOrWhiteSpace(key))
+                                {
+                                    key = "EMPTY " + j;
+                                }
+
+                                var value = tableContent[i][j];
+                                kvp.Add(key, value);
+                            }
+
+                            tableRows.Add(kvp);
+                        }
+
+                        map.Add($"Table{tableComponents.ElementAt(tableIndex).ID}", tableRows);
+                        tableIndex++;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            return map;
+        }
+
         public async Task<UploadData> ProcessUploadForIdSync(int uploadId, int supplierId)
         {
             // pdf content
             var upload = await _db.Upload.FirstOrDefaultAsync(u => u.ID == uploadId);
-            
+
             // get the regex list for the invoice template of supplier
             // suppose there is only one template for supplier
             var templateForSupplier = await _db.SupplierTemplateAssignment.Where(a => a.SupplierID == supplierId).Select(a => a.Template).FirstOrDefaultAsync();
             var fullRegexListForMatchingTemplate = await _db.RegexComponent.Where(rc => rc.TemplateID == templateForSupplier.ID).ToListAsync();
-           
+
             // fetch fields and save to db
             var fieldDataList = await _scraper.Scrape(upload.FilePath, fullRegexListForMatchingTemplate, upload);
             // fetch tables and save to db using a job
             var tableDataList = await _tableExtractor.ExtractTables(upload, templateForSupplier.ID);
-            
+
             var uploadData = new UploadData();
-            
+
             if (await _db.UploadData.AnyAsync(ud => ud.DetectedTemplateID == templateForSupplier.ID && ud.UploadID == upload.ID))
             {
                 uploadData = await _db.UploadData.FirstOrDefaultAsync(ud => ud.DetectedTemplateID == templateForSupplier.ID && ud.UploadID == upload.ID);
@@ -139,8 +227,8 @@ namespace HRMS_WEB.ApiControllers
             memoryStream.Position = 0;
             return new FormFile(memoryStream, 0, memoryStream.Length, fileName, fileName);
         }
-        
-        
+
+
         private string GetFileName(HttpResponseMessage response)
         {
             return Guid.NewGuid() + ".pdf";
